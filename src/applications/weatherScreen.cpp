901dc7ffc2e474ae30b2b7a563a78d9f4d5648b7
pablovepo <pablovepo77@gmail.com>
2025-11-13 16:49:43 -0500

    weather application changed

--- src/applications/weatherScreen.cpp
+++ src/applications/weatherScreen.cpp
@@ -11,89 +11,110 @@
 
 
 weatherScreen::weatherScreen(){
-
     link = "https://api.openweathermap.org/data/2.5/weather";
     key = "14a0c28171ba4e585da8f25871450521";
     this->appName = "weather";
     this->setObjectName("weatherScreen");
 
 }
+void weatherScreen::setup_layout(){
+    application = new QWidget;
+    this->application->setObjectName("weather");
+    top_weather_layout = new QVBoxLayout(application);
+    loc_layout = new QHBoxLayout();
+    time_date_layout = new QHBoxLayout();
+    temp_weather_layout = new QHBoxLayout();
+    next_temps = new QHBoxLayout();
+    multi_info_layout = new QHBoxLayout();
+    multi_info_widget = new QWidget();
+    final_layout = new QHBoxLayout();
+
+    top_weather_layout->addLayout(loc_layout);
+    top_weather_layout->addLayout(time_date_layout);
+    top_weather_layout->addLayout(temp_weather_layout);
+    multi_info_widget->setLayout(next_temps);
+    final_layout->addWidget(multi_info_widget,0, Qt::AlignLeft | Qt::AlignTop);
+    top_weather_layout->addLayout(final_layout);
+
+}
+
+void weatherScreen::setup_widgets(){
+    search_display = new QTextEdit("Search...", application);
+    time_date = new QLabel("Today, Nov 13, 13:51",application);
+    temp = new QLabel("16 C",application);
+    weather = new QLabel("Mostly Cloudy", application);
+    next_hour = new QLabel("17C", application);
+    seperator = new QFrame;
+    next_two_hour = new QLabel("19 C",application);
+
+    loc_layout->addWidget(search_display, 0, Qt::AlignLeft);
+    time_date_layout->addWidget(time_date, 0, Qt::AlignLeft);
+    temp_weather_layout->addWidget(temp,0,Qt::AlignLeft | Qt::AlignTop);
+    temp_weather_layout->addWidget(weather,0,Qt::AlignRight | Qt::AlignTop);
+    next_temps->addWidget(next_hour,0,Qt::AlignLeft);
+    next_temps->addWidget(seperator,0,Qt::AlignCenter);
+    next_temps->addWidget(next_two_hour, 0, Qt::AlignRight);
+}
+
+void weatherScreen::set_style_sheet(){
+    search_display->setStyleSheet("QTextEdit{"
+                                  "background-color: rgba(0,0,0,0);"
+                                  "color: #FFFFFF;"
+                                  "font-size: 24px;"
+                                  "border: none;"
+                                  "}");
+    application->setStyleSheet("QWidget#weather{"
+                               "background-image: url(../resources/appData/weather/cloudyDay.jpg);"
+                               "background-repeat: no-repeat;"
+                                "background-position: center;"
+                                "border: none;"
+                               "}");
+    time_date->setStyleSheet("QLabel{"
+                             "font-size: 12px;"
+                             "margin-top: 1ex;"
+                             "background: none;"
+                             "}");
+    temp->setStyleSheet("QLabel{"
+                        "color: rgba(255,255,255,199);"
+                        "font-size: 124px;"
+                        "background: none;"
+                        "}");
+    multi_info_widget->setStyleSheet("QWidget{"
+                                    "border: 2px solid white;"
+                                     "border-radius: 16px;"
+                                     "}");
+    next_hour->setStyleSheet("QLabel{"
+                             "border: none;}");
+    next_two_hour->setStyleSheet("QLabel{"
+                             "border: none;}");
+}
+void weatherScreen::set_styles(){
+    top_weather_layout->setMargin(20);
+    search_display->setFixedSize(130,55);
+    time_date->setFixedSize(150,45);
+    weather->setFixedSize(200,200);
+    time_date_layout->setContentsMargins(0,0,0,0);
+    temp_weather_layout->setContentsMargins(0,0,0,0);
+    multi_info_layout->setContentsMargins(0,0,0,0);
+    final_layout->setContentsMargins(0,0,0,0);
+    temp->setFixedSize(400,200);
+    multi_info_widget->setFixedSize(200,75);
+    final_layout->setContentsMargins(0,0,0,0);
+    seperator->setGeometry(QRect(320, 150, 118, 3));
+    seperator->setFrameShape(QFrame::VLine);
+    seperator->setFrameShadow(QFrame::Sunken);
+}
+
+void weatherScreen::setup_connections(){
+
+}
+
 
 void weatherScreen::btAPP_SETUP(){
-    this->application = new QWidget();
-    QVBoxLayout* mainScreen = new QVBoxLayout(this->application);
-    QLabel* cityName = new QLabel();
-    QLabel* weatherInfo = new QLabel();
-
-
-
-    QPushButton* enterInfo = new QPushButton("search");
-
-    QTextEdit* weatherSearch = new QTextEdit("Search...");
-    weatherSearch->setFixedWidth(200);
-    weatherSearch->setFixedHeight(35);
-    weatherSearch->setLineWrapMode(QTextEdit::NoWrap);
-    weatherSearch->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    weatherSearch->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    weatherSearch->setAlignment(Qt::AlignCenter);
-    weatherSearch->setStyleSheet("QTextEdit{"
-                                 "border: solid 5px #000000;"
-                                 "border-radius: 10px;"
-                                 "}");
-
-    enterInfo->setStyleSheet("QPushButton{"
-                             "padding: 15px;"
-                             "border-radius: 5px;"
-                             ""
-                             "}"
-                             "QPushButton:Hover{"
-                             "}");
-
-
-    connect(enterInfo, &QPushButton::clicked, this, [=]() {
-    std::string cityInput = weatherSearch->toPlainText().toStdString();
-
-    std::thread([=]() {
-        try {
-            cpr::Response r = cpr::Get(cpr::Url(
-                "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput +
-                "&appid=" + key + "&units=imperial"));
-            auto j = nlohmann::json::parse(r.text);
-            if (!j.contains("name") || !j.contains("main"))
-                throw std::runtime_error("bad json");
-
-            QString city = QString::fromStdString(j["name"]);
-            QString country = QString::fromStdString(j["sys"]["country"]);
-            double temp_double = j["main"]["temp"].get<double>();
-            QString temp_final = QString::number(temp_double) + " °F";
-
-            QMetaObject::invokeMethod(application, [=]() {
-                cityName->setText(city + ", " + country);
-                weatherInfo->setText(temp_final);
-            }, Qt::QueuedConnection);
-
-        } catch (...) {
-            QMetaObject::invokeMethod(application, [=]() {
-                cityName->setText("Error");
-                weatherInfo->setText("Invalid response");
-            }, Qt::QueuedConnection);
-        }
-    }).detach();
-});
-
-
-
-    mainScreen->addWidget(weatherSearch, 0, Qt::AlignCenter);
-    mainScreen->addWidget(cityName,0,Qt::AlignCenter);
-    mainScreen->addWidget(weatherInfo,0,Qt::AlignCenter);
-
-    mainScreen->addWidget(enterInfo,0,Qt::AlignCenter);
-
-    this->application->setLayout(mainScreen);
-    /*this->application->setStyleSheet("QWidget{"
-                  "background-color: rgba(255,255,255,0);"
-                  "color: rgba(0,0,0,255);"
-                  "}");*/
+    setup_layout();
+    setup_widgets();
+    set_styles();
+    set_style_sheet();
 }
 
 QString weatherScreen::returnAppName(){
--- src/applications/weatherScreen.h
+++ src/applications/weatherScreen.h
@@ -12,8 +12,52 @@ class weatherScreen : public btApplication {
     std::string toChangeInfo = "";
     std::string key;
     std::string link;
+
+    //------------ui_setup----------------------
+
+    QVBoxLayout* top_weather_layout;
+    QWidget* top_widget;
+    QHBoxLayout* loc_layout;
+    QHBoxLayout* time_date_layout;
+    QHBoxLayout* temp_weather_layout;
+    QHBoxLayout* next_temps;
+    QHBoxLayout* multi_info_layout;
+    QWidget* multi_info_widget;
+    QHBoxLayout* final_layout;
+        //--------widgets------------------
+
+    QTextEdit* search_display;
+    QLabel* time_date;
+    QLabel* temp;
+    QLabel* weather;
+    QLabel* next_hour;
+    QFrame* seperator;
+    QLabel* next_two_hour;
+
+
+    // ----------Multi info layouts--------------
+
+    QVBoxLayout* rain_chance_layout;
+    QLabel* rain_chance;
+
+    QVBoxLayout* wind_speed_layout;
+    QLabel* wind_speed;
+
+    QVBoxLayout* uv_layout;
+    QLabel* uv_info;
+
+
+
+    //------------------------------------------
+
     public:
     weatherScreen();
+    void setup_layout();
+    void setup_widgets();
+    void setup_connections();
+    void set_styles();
+    void set_style_sheet();
+
     void btAPP_SETUP() override;
     QWidget* btAPP_RETURN() override;
     void btAPP_CLOSED() override;
